e205a472012680b69db23d84e55b4ec1515cae53
SemihMutlu07 <117535331+SemihMutlu07@users.noreply.github.com>
2025-04-11 01:57:09 +0300

    real-fix for 404

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+    images: {
+        domains: ['srkswqjjdfkdddwemqtd.supabase.co'],
+      },
 };
 
 export default nextConfig;
