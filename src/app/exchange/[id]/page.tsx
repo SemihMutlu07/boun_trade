c937034a85a34b267cda48d1d895681bbae4552b
SemihMutlu07 <117535331+SemihMutlu07@users.noreply.github.com>
2025-04-10 14:14:40 +0300

    ?

--- src/app/exchange/[id]/page.tsx
+++ src/app/exchange/[id]/page.tsx
@@ -1,17 +1,30 @@
 import { notFound } from 'next/navigation';
 import {supabase} from '../../lib/supabase';
 import ProductDetailClient from './ProductDetailClient';
+import {Metadata } from 'next';
 
-export default async function ProductDetailPage({ params }: { params: { id: string} }) {
-  const { data: product, error } = await supabase
-    .from('products')
-    .select('*')
-    .eq('id', params.id)
-    .single()
-
-  if(!product || error ) {
-    notFound()
+type Props = {
+  params: {
+    id: string
   }
+}
 
-  return <ProductDetailClient product={product}/>
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+  return {
+    title: `Product ${params.id}`,
+  }
+}
+
+export default async function ProductDetailPage({params}: Props) {
+  const { data: product, error } = await supabase
+  .from('products')
+  .select('id, title, description, category, image_url, is_traded, user_id')
+  .eq('id', params.id)
+  .single()
+
+if (!product || error) {
+  notFound()
+}
+
+return <ProductDetailClient product={product} />
 }
